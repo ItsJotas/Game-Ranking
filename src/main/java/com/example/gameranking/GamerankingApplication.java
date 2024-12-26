6fa670fe60a69be2a6e1b15750e09a7ccca23a32
Jhonata Costa <costajhonata8@gmail.com>
2024-12-25 21:25:40 -0300

    #01[feature] - Initial configs

---
+++ src/main/java/com/example/gameranking/GamerankingApplication.java
@@ -0,0 +1,13 @@
+package com.example.gameranking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamerankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamerankingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gameranking/GamerankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gameranking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamerankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
