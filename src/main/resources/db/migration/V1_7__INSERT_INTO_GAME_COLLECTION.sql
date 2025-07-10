fa10ef186db2f55cba482b396c7cb26c72021dbe
Jhonata Costa <costajhonata8@gmail.com>
2025-07-10 17:09:57 -0300

    #43[feature] - Making an SQL script to create basic collections

---
+++ src/main/resources/db/migration/V1_7__INSERT_INTO_GAME_COLLECTION.sql
@@ -0,0 +1,15 @@
+INSERT INTO game_collection
+(ID, NAME, AVERAGE_RATING, COLOR)
+VALUES(1, 'All Achievements', 0.0, '#b4be1e');
+
+INSERT INTO game_collection
+(ID, NAME, AVERAGE_RATING, COLOR)
+VALUES(2, '100%', 0.0, '#707e88');
+
+INSERT INTO game_collection
+(ID, NAME, AVERAGE_RATING, COLOR)
+VALUES(3, 'Finished', 0.0, '#c8792b');
+
+INSERT INTO game_collection
+(ID, NAME, AVERAGE_RATING, COLOR)
+VALUES(4, 'No Campaign/Story Mode', 0.0, '#e7e7e7');
