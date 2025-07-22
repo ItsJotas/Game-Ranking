ba345fa7db2f2b05c994d4e82e6e90ef8df3cecc
Jhonata Costa <costajhonata8@gmail.com>
2025-07-22 19:57:36 -0300

    #46[fix] - Adding @Size and @NotNull validations to Game Collection creation

--- src/main/java/com/example/gameranking/model/dto/input/GameCollectionCreateDTO.java
+++ src/main/java/com/example/gameranking/model/dto/input/GameCollectionCreateDTO.java
@@ -1,5 +1,7 @@
 package com.example.gameranking.model.dto.input;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,8 +15,14 @@ import java.util.List;
 @NoArgsConstructor
 public class GameCollectionCreateDTO {
 
+    @NotNull(message = "The field Name cannot be null.")
+    @Size(max = 50, message = "The field Name cannot exceed 50 characters.")
     private String name;
+
     private List<Long> gameIds;
+
+    @NotNull(message = "The field Color cannot be null.")
+    @Size(max = 7, message = "The field Color must be a valid hex color code (e.g., #FFFFFF).")
     private String color;
 
 }
